5c5a2e0e6d56b5726d3970dcf13ef5166fef2f5c
miqueias199808@outlook.com <miqueias199808@outlook.com>
2025-01-15 16:36:02 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/navbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() => setMenu("home")} className={menu === "home" ? "activate" : ""}>
+          {" home "}
+        </li>
+        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "activate" : ""}>
+          menu
+        </li>
+        <li className={menu === "mobile - app" ? "activate" : ""}>mobile-app</li>
+        <li onClick={() => setMenu("contact us")} className="menu">
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sing in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
