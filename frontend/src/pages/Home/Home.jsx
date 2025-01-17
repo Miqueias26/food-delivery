1fa76efa9a346443e9324ec70c86c3dc905bcb24
miqueias199808@outlook.com <miqueias199808@outlook.com>
2025-01-17 17:28:15 +0100

    update 1.2

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../../components/Header/Header";
 import "./Home.css";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import { useState } from "react";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
