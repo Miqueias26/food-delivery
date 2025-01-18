796c31996d3230c4517a2210a068764ac1c9a823
miqueias199808@outlook.com <miqueias199808@outlook.com>
2025-01-18 19:08:17 +0100

    update 1.3

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./FoodDisplay.css";
 import { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
@@ -11,7 +12,7 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return;
+          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />;
         })}
       </div>
     </div>
